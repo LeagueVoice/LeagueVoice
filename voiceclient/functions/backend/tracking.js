cf65eb6e0f0670da0d862e4716746cf5a5aabc8d
Yuhan Fang <yuhan.fang@gmail.com>
2017-11-08 13:53:28 -0800

    Add stupid mock of whether user is tracked

---
+++ voiceclient/functions/backend/tracking.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+// Returns true if given unique ID is already tracked by us. Returns false
+// if it's a new user.
+userIsTracked = function(uniqueID) {
+  return false;
+}
