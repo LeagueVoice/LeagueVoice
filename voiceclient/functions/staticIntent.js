866afc22d29b42b64ae4b832166cde312e7bab40
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2017-11-08 23:02:44 -0800

    adding actual response for abilities

--- voiceclient/functions/staticIntent.js
+++ voiceclient/functions/staticIntent.js
@@ -1,22 +1,40 @@
 const rp = require('request-promise');
 
+function req(url) {
+  return rp(url)
+    .catch(e => rp(url));
+}
+
+const CDN = 'http://ddragon.leagueoflegends.com/cdn/';
+
 // "Cached" permanently.
-const versions = rp('https://ddragon.leagueoflegends.com/api/versions.json')
-  .catch(e => rp('https://ddragon.leagueoflegends.com/api/versions.json')) // 1 retry.
+const versions = req('https://ddragon.leagueoflegends.com/api/versions.json')
+  .then(JSON.parse);
+const latest = versions.then(versions => versions[0]);
+
+const champs = latest
+  .then(version => rp(`${CDN}${version}/data/en_US/champion.json`))
   .then(JSON.parse);
 
-const champs = versions
-  .then(versions => versions[0])
-  .then(version => rp(`http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`)
-    .catch(e => rp(`http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`)))
-  .then(JSON.parse);
+const abilitiesIndicies = [ 'Q', 'W', 'E', 'R', 'passive' ];
+
+function _getChampionAbilities(championKey) {
+  return latest
+    .then(version => req(`${CDN}${version}/data/en_US/champion/Aatrox.json`))
+    .then(JSON.parse)
+    .then(json => json.data[championKey].spells);
+}
 
 function championAbility(assistant) {
   return champs
     .then(champs => {
       let champion = assistant.getArgument('champion');
       let ability = assistant.getArgument('ability');
-      assistant.tell(`What is ${champion}'s ${ability}? I don't know`);
+      return _getChampionAbilities(champion)
+        .then(abilities => abilities[abilitiesIndicies.indexOf(ability)])
+        .then(ability => {
+          assistant.say(`${champion}'s ${ability} is ${ability.name}: ${ability.description}`);
+        });
     });
 }
 
