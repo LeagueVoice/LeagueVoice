8e5e79ffa365c1d6e14f764aedf183de4db12e14
ey1031 <eyudeveloper@gmail.com>
2017-11-08 16:23:07 -0800

    fixing function parameters in client

--- voiceclient/functions/backend/test.js
+++ voiceclient/functions/backend/test.js
@@ -3,3 +3,7 @@ const foo = require('./client.js');
 foo.getBySummonerName('waddlechirp', 'na1').then(function(res) {
   console.log(res);
 })
+
+foo.getRecentMatchList(237254272, "na1").then(function(res) {
+	console.log(res)
+})
