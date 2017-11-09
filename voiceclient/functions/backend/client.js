de9b1afff6663867ff7049cf231be7229830ed52
Yuhan Fang <yuhan.fang@gmail.com>
2017-11-08 16:12:49 -0800

    Add proxy functions for matchlist and match.

--- voiceclient/functions/backend/client.js
+++ voiceclient/functions/backend/client.js
@@ -14,7 +14,31 @@ getBySummonerName = function(name, region) {
   return rp(options)
 }
 
+// Returns a promise for getRecentMatchList that resolves to the returned JSON
+// from the Riot API matchlists/by-account/./recent endpoint.
+getRecentMatchList = function(accountID) {
+  const options = {
+    method: 'GET',
+    uri: APIPROXY + '/rgapi/' + region + '/match/getRecentMatchlist/' + accountID,
+    json: true 
+  }
+  return rp(options)
+}
+
+// Returns a promise for getMatch that resolves to the returned JSON from the
+// Riot API matches/. endpoint.
+getMatch = function(matchID) {
+  const options = {
+    method: 'GET',
+    uri: APIPROXY + '/rgapi/' + region + '/match/getMatch/' + matchID,
+    json: true 
+  }
+  return rp(options)
+}
+
 module.exports = {
-  "getBySummonerName": getBySummonerName
+  "getBySummonerName": getBySummonerName,
+  "getRecentMatchList": getRecentMatchList, 
+  "getMatch": getMatch
 }
 
