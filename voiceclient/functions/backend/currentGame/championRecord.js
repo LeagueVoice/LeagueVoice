1909ffd6303e81a2a41166574986fb5e1dc3f990
Yuhan Fang <yuhan.fang@gmail.com>
2017-11-10 01:01:02 -0800

    Add champion record fetch.

--- voiceclient/functions/backend/currentGame/championRecord.js
+++ voiceclient/functions/backend/currentGame/championRecord.js
@@ -1,4 +1,5 @@
 const firebase = require('firebase');
+const client = require('../client.js');
 
 // For the game that the user ID is in, return the record for the given
 // champion ID in that game.
@@ -12,10 +13,52 @@ getChampionRecord = function(uniqueID, championID) {
           let participant = match.participants.find(function(elem) {
             return elem.championId == championID;
           });
-          return participant.summonerId;
+          return {
+            id: participant.summonerId,
+            region
+          };
         });
-      }).then(function(summonerID) {
-        // What's the record for summonerID for championID?
+      }).then(function(participant) {
+        return client.getBySummonerId(participant.id, participant.region)
+          .then(function(res) {
+            return {
+              id: res.accountId,
+              region: participant.region
+            };
+          });
+      }).then(function(participant) {
+        return client.getMatchlist(participant.id, participant.region, championID)
+          .then(function(res) {
+            return Promise.all(res.matches.map(function(e) {
+              return client.getMatch(e.gameId, participant.region);
+            })).then(function(matches) {
+              return {
+                participant,
+                matches
+              };
+            });
+          });
+      }).then(function(matches) {
+        let isWin = matches.matches.map(function(match) {
+          let participantID = match.participantIdentities.find(function(i) {
+            return i.player.accountId == matches.participant.id;
+          }).participantId;
 
+          let participant = match.participants.find(function(i) {
+            return i.participantId == participantID;
+          });
+
+          let team = match.teams.find(function(i) {
+            return i.teamId == participant.teamId;
+          });
+
+          return team.win == 'Win';
+        });
+
+        return isWin.reduce((a, b) => a + b, 0) / isWin.length;
       });
 };
+
+module.exports = {
+  getChampionRecord
+}
