59f14a3bfb9bae42b0ac3c007b747bfc2437cac5
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2017-11-23 00:50:08 -0800

    clean up tipsIntent

--- voiceclient/functions/tipsIntent.js
+++ voiceclient/functions/tipsIntent.js
@@ -1,47 +1,29 @@
-const enemyTips = require('./backend/userNotes/enemyTips')
-const client = require('./backend/client')
-const user = require('./firebase/user')
+const enemyTips = require('./backend/userNotes/enemyTips');
+const client = require('./backend/client');
+const user = require('./firebase/user');
 
 const EnemyTipsIntent = (app) => {
 	user.getById(app.getUser()["user_id"])
-	.then(function(snapshot){
-		console.log(snapshot)
-		console.log(snapshot.summonerID)
-		client.getCurrentMatch(snapshot.summonerID)
-		.then(function(match){
-			let participant = match.participants.find(function(elem) {
- 	           return elem.summonerId == snapshot.summonerID;
- 	        })
- 	        //console.log(client.getChampionName(participant.championId))
-			enemyTips.getTipsAgainst(app.getArgument('champion'), client.getChampionName(participant.championId))
-			.then(function(response){
-				//console.log(response)
-				if (response.length !== 0) {
-					app.ask("According to LoL Counter, " + response[0])
-				}
-				else {
-					app.tell("I could not find reliable tips from LoL Counter for this champ! Ripperoni.")
-				}
-			})
-			.catch(function(e){
-				console.log(e)
-			})
+		.then(snapshot => {
+			console.log(snapshot);
+			console.log(snapshot.summonerID);
+			return client.getCurrentMatch(snapshot.summonerID);
 		})
-		.catch(function(e, match){
-			enemyTips.getTipsAgainst(app.getArgument('champion'))
-			.then(function(response){
-				console.log(response)
-				if (response.length !== 0) {
-					app.ask("According to LoL Counter, " + response[0])
-				}
-				else {
-					app.tell("I could not find reliable tips from LoL Counter for this champ! Ripperoni.")
-				}			
-			})
+		.then(match => {
+			let participant = match.participants.find(elem => elem.summonerId == snapshot.summonerID);
+			return enemyTips.getTipsAgainst(app.getArgument('champion'), client.getChampionName(participant.championId));
 		})
-	})
+		.catch(e => enemyTips.getTipsAgainst(app.getArgument('champion')))
+		.then(response => {
+			if (response.length !== 0) {
+				app.ask("According to LoL Counter, " + response[0]);
+			}
+			else {
+				app.tell("I could not find reliable tips from LoL Counter for this champ! Ripperoni.");
+			}
+		});
 }
 
 module.exports = {
 	EnemyTipsIntent
-}
+};
