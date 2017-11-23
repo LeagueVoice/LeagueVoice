3fe39a65292f5fd7b357eef33d407b879114f1fd
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2017-11-23 00:53:09 -0800

    clean up championSelect

--- voiceclient/functions/backend/championSelect/championSelect.js
+++ voiceclient/functions/backend/championSelect/championSelect.js
@@ -1,8 +1,8 @@
-const tracking = require('../tracking')
-const client = require('../client')
-const helper = require('../helper')
-const fbUser = require('../../firebase/user')
-const aggregate = require('../aggregate')
+const tracking = require('../tracking');
+const client = require('../client');
+const helper = require('../helper');
+const fbUser = require('../../firebase/user');
+const aggregate = require('../aggregate');
 
 const championSelect = {
   /**
@@ -11,27 +11,26 @@ const championSelect = {
    * b) amount of championMastery points the user has for the champions
    * @param userPosition 'MIDDLE' | 'TOP' | 'JUNGLE' | 'DUO_CARRY' | 'DUO_SUPPORT'
    */
-  suggestChampionToPick: function (uniqueID, userPosition) {
+  suggestChampionToPick: function(uniqueID, userPosition) {
     // get all champions from .gg for the role
-    const userChampionMasteriesPromise = aggregate.userChampionMasteries(uniqueID)
-    const championsforRoleFromGGPromise = fbUser.getById(uniqueID).then(user => {
-      return client.getGGChampionsForRole(userPosition, 'BRONZE') // TODO: actually store users tier
-    })
-    return Promise.all([
-      userChampionMasteriesPromise,
-      championsforRoleFromGGPromise
-    ]).then(([championMasteries, championGGChampions]) => {
-      const championMasteriesIDs = championMasteries.map(cmastery => cmastery.championId)
-      const championGGIds = championGGChampions.map(champion => champion.championId)
+    const userChampionMasteriesPromise = aggregate.userChampionMasteries(uniqueID);
+    const championsforRoleFromGGPromise = fbUser.getById(uniqueID)
+      .then(user => client.getGGChampionsForRole(userPosition, 'BRONZE')); // TODO: actually store users
+    return Promise
+      .all([
+        userChampionMasteriesPromise,
+        championsforRoleFromGGPromise
+      ])
+      .then(([championMasteries, championGGChampions]) => {
+        const championMasteriesIDs = championMasteries.map(cmastery => cmastery.championId);
+        const championGGIds = championGGChampions.map(champion => champion.championId);
 
-      // match championMastery with highest winrate on champion.gg
-      return championGGIds
-        .sort((championIdA, championIdB) => {
-          return helper.sortByAppearanceInArray(championMasteriesIDs, championIdA, championIdB)
-        })
-        .slice(0, 5)
-    })
+        // match championMastery with highest winrate on champion.gg
+        return championGGIds
+          .sort((champIdA, champIdB) => helper.sortByAppearanceInArray(championMasteriesIDs, champIdA, champIdB))
+          .slice(0, 5);
+      });
   }
-}
+};
 
-module.exports = championSelect
+module.exports = championSelect;
