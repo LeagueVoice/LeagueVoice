11d371fbfa7fda0d35ac08220bc238167f0cda0a
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2017-11-23 00:34:39 -0800

    clean up voiceclient itemIntent.js

--- voiceclient/functions/itemIntent.js
+++ voiceclient/functions/itemIntent.js
@@ -1,23 +1,21 @@
-const classification = require('./backend/itemization/classification')
-const championRole = require('./backend/itemization/championRole')
+const classification = require('./backend/itemization/classification');
+const championRole = require('./backend/itemization/championRole');
 
 const ItemSuggestion = (app) => {
 	classification.getItems(app.getUser()['user_id'], app.getArgument('status'), app.getArgument('situation'))
-	.then(function(item_list) {
-		var i = 0
-		var speak_string = "Think about buying these items: "
-		for (; i < item_list.length-2; i++) {
-				speak_string+= item_list[i] + ', '
+		.then(itemList => {
+			let i = 0;
+			let speakString = "Think about buying these items: ";
+			for (; i < item_list.length - 2; i++) {
+				speakString += itemList[i] + ', ';
 			}
-			speak_string+= item_list[i] + " " + item_list[i+1]
-			speak_string+= ". Good luck!"
-			app.ask(speak_string)
+			speakString += itemList[i] + " " + itemList[i + 1];
+			speakString += ". Good luck!";
+			app.ask(speakString);
 		})
-	.catch(function(e) {
-		app.tell("I can't suggest an item for you right now. Set up your summoner with me and make sure you're in a game.")
-	})
-}
+		.catch(e => app.tell("I can't suggest an item for you right now. Set up your summoner with me and make sure you're in a game."));
+};
 
 module.exports = {
 	ItemSuggestion
-}
+};
