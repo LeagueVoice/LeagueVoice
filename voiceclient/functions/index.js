4db22fe7e3e477646ff19e015220aa5497e402e4
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2017-11-23 00:31:54 -0800

    cleaning up function index.js

--- voiceclient/functions/index.js
+++ voiceclient/functions/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const DialogflowApp = require('actions-on-google').DialogflowApp;
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const functions = require('firebase-functions');
 const tracking = require('./backend/tracking.js');
 const client = require('./backend/client.js');
@@ -9,8 +9,8 @@ const champselect = require('./backend/championSelect/championSelect.js');
 
 const gameTimer = require('./backend/currentGame/gameTimer.js');
 const firebase = require('firebase');
-const fbUser = require('./firebase/user')
-const aggregate = require('./backend/aggregate')
+const fbUser = require('./firebase/user');
+const aggregate = require('./backend/aggregate');
 const spell = require('./backend/currentGame/spellTimer.js');
 const tipBackend = require('./backend/userNotes/enemyTips.js');
 
@@ -18,12 +18,16 @@ const staticIntent = require('./staticIntent');
 const notesIntent = require('./notesIntent');
 const matchIntent = require('./matchIntent');
 const itemIntent = require('./itemIntent');
-const championRole = require('./backend/itemization/championRole')
-const tipsIntent = require('./tipsIntent')
+const championRole = require('./backend/itemization/championRole');
+const tipsIntent = require('./tipsIntent');
+
+function capitalize(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
 
 const welcomeIntent = (app) => {
-    app.ask("Welcome to League Voice! How can we help you improve?")
-}
+    app.ask("Welcome to League Voice! How can we help you improve?");
+};
 
 const checkUserRanksIntent = (app) => {
   const numeralEnum = {
@@ -32,85 +36,74 @@ const checkUserRanksIntent = (app) => {
     "III": "3",
     "IV": "4",
     "V": "5"
-  }
+  };
   aggregate.userRanksByQueue(app.getUser()['user_id'])
-    .then(function(res){
-      var rankArray = res["RANKED_SOLO_5x5"].split(" ")
-      var rankStr = rankArray[0].toLowerCase() + " " + numeralEnum[rankArray[1]]
-      if (rankArray[0] !== "CHALLENGER"){
-        app.tell("You're a " + rankStr + " player. Let's work to get you even higher!")
+    .then(res => {
+      let rankArray = res["RANKED_SOLO_5x5"].split(" ")
+      let rankStr = rankArray[0].toLowerCase() + " " + numeralEnum[rankArray[1]]
+      if (rankArray[0] !== "CHALLENGER") {
+        app.tell("You're a " + rankStr + " player. Let's work to get you even higher!");
       }
       else {
-        app.tell("You're a " + rankStr + " player. Please teach me how to play, senpai.")
+        app.tell("You're a " + rankStr + " player. Please teach me how to play, senpai.");
       }
-  })
-    .catch(function(e) {
-      app.tell("I can't get your rank right now. Set up your summoner with me first.")
-    });
-}
+    })
+    .catch(e => app.tell("I can't get your rank right now. Set up your summoner with me first."));
+};
 
 const WhoToPlayAgainstIntent = (app) => {
-  console.log(client.getChampionID(app.getArgument('champion').toLowerCase()))
+  //console.log(client.getChampionID(app.getArgument('champion').toLowerCase()));
   client.getBestMatchupsByLane(client.getChampionID(app.getArgument('champion').toLowerCase()))
-  .then(function(response){
-    console.log(response)
-    if (response[0].count != 0){
-      var name = client.getChampionName(response[0].matchups[0].championID)
-      var nice_name = name.charAt(0).toUpperCase() + name.slice(1)
-      app.ask("You should play " + nice_name + ". " + nice_name + " has a " + Math.round(response[0].matchups[0].winrate*100) + " percent winrate in this matchup.");
-    }
-    else {
-      app.tell("Welp, I have no clue. Play what feels best!");
-    }
-  });
-}
+    .then(response => {
+      //console.log(response);
+      if (response[0].count != 0) {
+        var name = capitalize(client.getChampionName(response[0].matchups[0].championID));
+        app.ask(`You should play ${name}. ${name} has a ${Math.round(response[0].matchups[0].winrate * 100)} percent winrate in this matchup.`);
+      }
+      else {
+        app.tell("Welp, I have no clue. Play what feels best!");
+      }
+    });
+};
 
 const RoleChampSuggestIntent = (app) => {
   champselect.suggestChampionToPick(app.getUser()["userId"], app.getArgument('role').toUpperCase())
-    .then(function(response){
-      console.log(response)
-      var champString = ""
-      var name;
-      var nice_name;
-      var i;
-      for (i = 0; i < response.length - 1; i++) {
-        name = client.getChampionName(response[i])
-        nice_name = name.charAt(0).toUpperCase() + name.slice(1)
-        champString += nice_name + ", ";
+    .then(response => {
+      //console.log(response)
+      let champStrings = [];
+      let name, niceName;
+      let i = 0;
+      for (; i < response.length; i++) {
+        name = capitalize(client.getChampionName(response[i]));
+        champStrings.push(name);
       }
-      name = client.getChampionName(response[i])
-      nice_name = name.charAt(0).toUpperCase() + name.slice(1)
-      champString += "or " + nice_name
-      app.ask("Based on your mastery and current winrate, some champs you could play are " + champString)
-  })
-    .catch(function(e) {
-      app.tell("We can't suggest champions for you right now. Make sure that you've registered your summoner with me.")
+      champStrings[champStrings.length - 1] = 'or ' + name;
+      app.ask(`Based on your mastery and current winrate, some champs you could play are ${champStrings.join(', ')}.`);
     })
-}
+    .catch(e => app.tell("I can't suggest champions for you right now. Make sure that you've registered your summoner with me."));
+};
 
 const WhoToBanIntent = (app) => {
   client.getBestMatchupsByLane(client.getChampionID(app.getArgument('champion').toLowerCase()))
-  .then(function(response){
-if (response[0].count != 0){
-      var name = client.getChampionName(response[0].matchups[0].championID)
-      var nice_name = name.charAt(0).toUpperCase() + name.slice(1)
-      app.ask("You should ban " + nice_name + ". " + nice_name + " has a " + Math.round(response[0].matchups[0].winrate*100) + " percent winrate in this matchup.");
-    }
-    else {
-      app.tell("Welp, I have no clue. Play what feels best!");
-    }
-  });
-}
+    .then(response => {
+      if (response[0].count != 0){
+        var name = capitalize(client.getChampionName(response[0].matchups[0].championID));
+        app.ask(`How about banning ${name}? ${name} has a ${Math.round(response[0].matchups[0].winrate * 100)} percent winrate in this matchup.`);
+      }
+      else {
+        app.tell("Welp, I have no clue. Play what feels best!");
+      }
+    });
+};
 
 const SummonerIntent = (app) => {
-  app.ask("Your summoner name is set to: " + app.getArgument('summoner') + ". What region do you play in?")
-}
+  app.ask("Your summoner name is set to: " + app.getArgument('summoner') + ". What region do you play in?");
+};
 
 const RegionIntent = (app) => {
-  fbUser.createFromSummonerName(app.getUser()['userId'], app.getArgument('summoner'), app.getArgument('region')).then(function(res){
-    app.ask("Your region is set to: " + app.getArgument('region') + ".")
-  });
-}
+  fbUser.createFromSummonerName(app.getUser()['userId'], app.getArgument('summoner'), app.getArgument('region'))
+    .then(ignoredResponse => app.ask(`Your region is set to: ${app.getArgument('region')}.`));
+};
 
 const Actions = { // the action names from the DialogFlow intent. actions mapped to functions
     WELCOME_INTENT: 'input.welcome',
@@ -134,12 +127,12 @@ const Actions = { // the action names from the DialogFlow intent. actions mapped
     READ_NOTE: 'ReadNote',
     ITEM_SUGGESTION: 'ItemWinLoseEqual',
     ENEMY_TIPS: 'EnemyTips'
-}
+};
 
 function initialize() {
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyB9sQFV5h8cK3kDGkMtKy4-6RK3x7Aados",
+  let config = {
+    apiKey: "AIzaSyB9sQFV5h8cK3kDGkMtKy4-6RK3x7Aados", // uh
     authDomain: "league-voice-7fa50.firebaseapp.com",
     databaseURL: "https://league-voice-7fa50.firebaseio.com",
     projectId: "league-voice-7fa50",
@@ -150,6 +143,7 @@ function initialize() {
 }
 
 initialize();
+
 const actionMap = new Map();
 actionMap.set(Actions.WELCOME_INTENT, welcomeIntent);
 actionMap.set(Actions.CHECK_USER_RANKS, checkUserRanksIntent);
@@ -173,25 +167,6 @@ actionMap.set(Actions.READ_NOTE, notesIntent.ReadNoteIntent);
 actionMap.set(Actions.ITEM_SUGGESTION, itemIntent.ItemSuggestion)
 actionMap.set(Actions.ENEMY_TIPS, tipsIntent.EnemyTipsIntent)
 
-/* 
-fbUser.createFromSummonerName("testfang", "45620", "na1").then(function(res) {
-  championRecord.getChampionRecord("testfang", client.getChampionID("jinx")).then(console.log);
-});
-*/
-
-// classification.getItems('test3');
-// checkUserRanksIntent("test").then(function(response){
-// 	console.log(JSON.stringify(response));
-// }).catch(function(e){
-// 	console.log(e);
-// });
-
-
-
-//tracking.createUser(97, "orkosarkar", "na1")
-//tracking.addNewMatches("test2", 230957428, "na1")
-
-//spell.getSpellTime('test', 'annie', 'flash').then(snap=>console.log(snap));
 
 const leagueVoice = functions.https.onRequest((request, response) => {
   const app = new DialogflowApp({request, response});
@@ -201,9 +176,3 @@ const leagueVoice = functions.https.onRequest((request, response) => {
 module.exports = {
   leagueVoice
 };
-
-// tipBackend.getTipsAgainst("ashe", "jhin").then(function(tips){
-//   console.log(tips)
-// })
-
-
