d8055e5f5c2bcc674c111998f564c9f87b6c04ac
Mingwei Samuel <Mingwei.Samuel@gmail.com>
2017-11-23 00:41:43 -0800

    clean up voiceclient notesintent.js

--- voiceclient/functions/notesIntent.js
+++ voiceclient/functions/notesIntent.js
@@ -1,21 +1,17 @@
-const championNotes = require('./backend/userNotes/championNotes')
+const championNotes = require('./backend/userNotes/championNotes');
 
 const WriteNoteIntent = (app) => {
-	championNotes.addChampionNote(app.getUser().userId, app.getArgument('champion'), 0, app.getArgument('note'))
-	app.ask('Okay. Next time you play ' + app.getArgument('champion') + ' ask me to remind you ' + app.getArgument('note'))
-}
+	championNotes.addChampionNote(app.getUser().userId, app.getArgument('champion'), 0, app.getArgument('note'));
+	app.ask('Okay. Next time you play ' + app.getArgument('champion') + ' ask me to remind you ' + app.getArgument('note'));
+};
 
 const ReadNoteIntent = (app) => {
 	championNotes.getChampionNotes(app.getUser().userId, app.getArgument('champion'))
-	.then(function(note) {
-		app.ask(note)
-	})
-	.catch(function(e) {
-		app.tell("Sorry, we can't read notes for you right now. Make sure that you've registered your summoner with me.")
-	})
-}
+		.then(note => app.ask(note))
+		.catch(e => app.tell("Sorry, I can't read notes for you right now. Make sure that you've registered your summoner with me."));
+};
 
 module.exports = {
 	WriteNoteIntent,
 	ReadNoteIntent
-}
+};
